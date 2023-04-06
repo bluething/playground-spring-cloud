b8f606d9de8459d2b40d0a56415cc65ab2819ecd
Habib Ali Machpud <habib.machpud@gmail.com>
2023-04-06 12:45:42 +0700

    Configure security config at config server

--- config/config-server-file-encrypt/src/main/java/io/github/bluething/playground/spring/cloud/Main.java
+++ config/config-server-file-encrypt/src/main/java/io/github/bluething/playground/spring/cloud/Main.java
@@ -3,11 +3,29 @@ package io.github.bluething.playground.spring.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @EnableConfigServer
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+}
+
+@Configuration
+class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests((authz) ->
+                        authz.requestMatchers("/decrypt").denyAll()
+                                .anyRequest().authenticated())
+                .csrf().disable()
+                .httpBasic();
+        return httpSecurity.build();
     }
 }
